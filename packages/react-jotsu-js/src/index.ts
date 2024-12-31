37a95bff2ea14a8f99cf9079d887cb52c30c5c93
Matthew R. Laue <mattlaue@gmail.com>
2024-12-31 07:46:25 -0800

    Change the exports in react-jotsu-js

--- packages/react-jotsu-js/src/index.ts
+++ packages/react-jotsu-js/src/index.ts
@@ -1,9 +1,11 @@
 // noinspection JSUnusedGlobalSymbols
 
-export { default as HelloWorld } from './components/HelloWorld';
-export { default as AccountDescription } from './components/AccountDescription';
+import HelloWorld from './components/HelloWorld';
+import AccountDescription from './components/AccountDescription';
 
-export { default as Register } from './components/auth/register/Register';
-export { default as Login } from './components/auth/login/Login';
-export { default as ForgotPassword } from './components/auth/forgotPassword/ForgotPassword';
-export { default as ResetPassword } from './components/auth/resetPassword/ResetPassword';
+import Register from './components/auth/register/Register';
+import Login from './components/auth/login/Login';
+import ForgotPassword from './components/auth/forgotPassword/ForgotPassword';
+import ResetPassword from './components/auth/resetPassword/ResetPassword';
+
+export { HelloWorld, AccountDescription, Register, Login, ForgotPassword, ResetPassword };
