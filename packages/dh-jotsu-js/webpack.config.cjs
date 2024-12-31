dab11075f826652da8dbe3661f72b9cb99861a6e
Matthew R. Laue <mattlaue@gmail.com>
2024-12-31 07:02:47 -0800

    Lerna and nohoist
    Squashed commit of the following:
    
    commit 922171562a685fb46668a2a471a60f84d28fce5e
    Author: Matthew R. Laue <mattlaue@gmail.com>
    Date:   Tue Dec 31 07:01:54 2024 -0800
    
        Add lerna.
    
    commit 6134df1b15d17837ff48307b696ac08a0ba0cfc7
    Author: Matthew R. Laue <mattlaue@gmail.com>
    Date:   Tue Dec 31 06:53:45 2024 -0800
    
        Don't hoist react/preact and switch to npm.

--- packages/dh-jotsu-js/webpack.config.cjs
+++ packages/dh-jotsu-js/webpack.config.cjs
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const TOPDIR = path.dirname(path.dirname(__dirname));
+
 module.exports = {
     mode: 'development',
     devtool : 'source-map',
@@ -21,10 +23,10 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
         alias: {
-            'react': path.resolve(__dirname, 'node_modules', 'preact/compat'),
-            'react-dom/test-utils': path.resolve(__dirname, 'node_modules', 'preact/test-utils'),
-            'react-dom': path.resolve(__dirname, 'node_modules', 'preact/compat'),
-            'react/jsx-runtime': path.resolve(__dirname, 'node_modules', 'preact/jsx-runtime'),
+            'react': path.resolve(TOPDIR, 'node_modules', 'preact/compat'),
+            'react-dom/test-utils': path.resolve(TOPDIR, 'node_modules', 'preact/test-utils'),
+            'react-dom': path.resolve(TOPDIR, 'node_modules', 'preact/compat'),
+            'react/jsx-runtime': path.resolve(TOPDIR, 'node_modules', 'preact/jsx-runtime'),
         },
         modules: [path.resolve(__dirname, 'src'), 'node_modules']
     }
