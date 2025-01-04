08b72b06f02e3dc0d64710ed9a9c930afb8dd386
Matthew R. Laue <mattlaue@gmail.com>
2025-01-04 13:40:58 -0800

    Better error handling in useConfirmEmail.

--- packages/react-jotsu-js/src/hooks/useConfirmEmail.ts
+++ packages/react-jotsu-js/src/hooks/useConfirmEmail.ts
@@ -6,15 +6,16 @@ export function useConfirmEmail(apiClient: Client): User | Error | undefined {
     const [user, setUser] = useState<User>();
     const [error, setError] = useState<string>();
 
-    const token = getFirstQueryParam('t');
-
     useEffect(() => {
+        const token = getFirstQueryParam('t');
         if (token) {
             confirmEmail(apiClient, { token })
                 .then((user) => setUser(user))
                 .catch((e) => setError(getErrorDetail(e)));
+        } else {
+            setError('Missing query parameter "t"');
         }
-    }, [apiClient, token]);
+    }, [apiClient]);
 
     return error ? Error(error) : user;
 }
