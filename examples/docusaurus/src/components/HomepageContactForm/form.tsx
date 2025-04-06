787d21c0df2d0c2a7c9e26983c88943c8257abde
Matthew R. Laue <mattlaue@gmail.com>
2025-04-06 09:00:24 -0700

    Make the contact form browser-only.

---
+++ examples/docusaurus/src/components/HomepageContactForm/form.tsx
@@ -0,0 +1,45 @@
+import { ReactNode, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Form } from '@jotsu/react-jotsu-js';
+import styles from './styles.module.css';
+
+type Inputs = {
+    email: string
+}
+
+function HomepageContactFormBrowserOnly(): ReactNode {
+    const [submitted, setSubmitted] = useState(false);
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<Inputs>();
+
+    if (submitted) {
+        return (
+            <section className={styles.section}>
+                <p className={styles.submitted}>Thank you for your submission!</p>
+            </section>
+        );
+    }
+
+    return (
+        <section className={styles.section}>
+            <Form
+                className={styles.form}
+                accountId={'1'}
+                name={'contact'}
+                handleSubmit={handleSubmit}
+                onReset={() => reset()}
+                onSubmitSuccess={() => setSubmitted(true)}
+            >
+                <h2>Contact Us: Jotsu Form</h2>
+                <div className={errors.email ? 'error' : undefined}>
+                    <label className={'field-help'} htmlFor={'email'}>Email <span aria-required="true">*</span></label>
+                    <input id={'email'} {...register('email', { required: true })} />
+                    {errors.email && <span className={'field-help'}>This field is required</span>}
+                </div>
+                <input type={'submit'} />
+                <input type={'reset'} />
+            </Form>
+        </section>
+    );
+}
+
+export default HomepageContactFormBrowserOnly;
--- examples/docusaurus/src/components/HomepageContactForm/index.tsx
+++ examples/docusaurus/src/components/HomepageContactForm/index.tsx
@@ -1,43 +1,12 @@
-import { ReactNode, useState } from 'react';
-import { useForm } from "react-hook-form";
-import { Form } from '@jotsu/react-jotsu-js';
-import styles from './styles.module.css';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
-type Inputs = {
-    email: string
-}
-
-export default function HomepageContactForm(): ReactNode {
-    const [submitted, setSubmitted] = useState(false);
-    const {register, handleSubmit, formState: { errors }, reset} = useForm<Inputs>();
-
-    if (submitted) {
-        return (
-            <section className={styles.section}>
-                <p className={styles.submitted}>Thank you for your submission!</p>
-            </section>
-        );
-    }
-
+export default function HomepageContactForm() {
     return (
-        <section className={styles.section}>
-            <Form
-                className={styles.form}
-                accountId={'1'}
-                name={'contact'}
-                handleSubmit={handleSubmit}
-                onReset={() => reset()}
-                onSubmitSuccess={() => setSubmitted(true)}
-            >
-                <h2>Contact Us: Jotsu Form</h2>
-                <div className={errors.email ? 'error' : undefined}>
-                    <label className={'field-help'} htmlFor={'email'}>Email <span aria-required="true">*</span></label>
-                    <input id={'email'} {...register('email', { required: true })} />
-                    {errors.email && <span className={'field-help'}>This field is required</span>}
-                </div>
-                <input type={'submit'} />
-                <input type={'reset'} />
-            </Form>
-        </section>
+        <BrowserOnly>
+            {() => {
+                const HomepageContactFormBrowserOnly = require('./form.tsx').default;
+                return <HomepageContactFormBrowserOnly />;
+            }}
+        </BrowserOnly>
     );
 }
