fbcdaf5f3f4679e6313a179720b87303a06be8d1
Matthew R. Laue <mattlaue@gmail.com>
2025-05-14 11:11:46 -0700

    Add the JOTSU_ACCOUNT_ID env var to the cloudflare-worker-bff example.

--- examples/cloudflare-worker-bff/src/index.ts
+++ examples/cloudflare-worker-bff/src/index.ts
@@ -2,18 +2,23 @@ interface HeadersWithGetSetCookie extends Headers {
 	getSetCookie(): string[];
 }
 
+interface EnvType extends Env {
+	JOTSU_ACCOUNT_ID: string | undefined;
+}
+
 /*
  * NOTE: this DOES NOT work locally, i.e. when using 'wrangler dev'.
  * Cloudflare return 405, Error 1000: DNS points to prohibited IP.
  */
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
+	async fetch(request, env): Promise<Response> {
 		const url = new URL(request.url);
 
+		const jotsu_account_id = (env as EnvType).JOTSU_ACCOUNT_ID || '1';
 		const hostname = 'api.jotsu.com';
 		const pathname = url.pathname.replace('/api', '');
 
-		const targetUrl = `https://${hostname}/1${pathname}${url.search}`;
+		const targetUrl = `https://${hostname}/${jotsu_account_id}${pathname}${url.search}`;
 		let proxyResponse = await fetch(targetUrl, request);
 
 		const cfHeaders = proxyResponse.headers as HeadersWithGetSetCookie;
