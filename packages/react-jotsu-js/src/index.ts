228f86bba8fb631e4edbaad0ad6c64f1c6530b56
Matthew R. Laue <mattlaue@gmail.com>
2025-05-28 11:15:52 -0700

    Export FormGroup too.

--- packages/react-jotsu-js/src/index.ts
+++ packages/react-jotsu-js/src/index.ts
@@ -1,21 +1,29 @@
 // noinspection JSUnusedGlobalSymbols
 
 import HelloWorld from './components/HelloWorld';
-import AccountDescription, {type AccountDescriptionProps} from './components/AccountDescription';
+import AccountDescription, { type AccountDescriptionProps } from './components/AccountDescription';
 
-import Register, {type RegisterProps} from './components/auth/register/Register';
-import Login, {type LoginProps} from './components/auth/login/Login';
+import Register, { type RegisterProps } from './components/auth/register/Register';
+import Login, { type LoginProps } from './components/auth/login/Login';
 import ForgotPassword from './components/auth/forgotPassword/ForgotPassword';
 import ResetPassword from './components/auth/resetPassword/ResetPassword';
 import ConfirmEmail from './components/auth/confirmEmail/ConfirmEmail';
 
-import Form, {type FormProps} from './components/forms/Form';
+import Form, { type FormProps } from './components/forms/Form';
+import FormGroup from './components/forms/FormGroup';
 
 export {
     HelloWorld,
-    AccountDescription, AccountDescriptionProps,
-    Register, RegisterProps,
-    Login, LoginProps,
-    ForgotPassword, ResetPassword, ConfirmEmail,
-    Form, FormProps,
+    AccountDescription,
+    AccountDescriptionProps,
+    Register,
+    RegisterProps,
+    Login,
+    LoginProps,
+    ForgotPassword,
+    ResetPassword,
+    ConfirmEmail,
+    Form,
+    FormGroup,
+    FormProps,
 };
