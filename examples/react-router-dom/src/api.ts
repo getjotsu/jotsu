8f1a96e2cf06add9c434f9aa2a3fae882d82cfc3
Matthew R. Laue <mattlaue@gmail.com>
2025-05-29 07:45:55 -0700

    Update the react-router-dom example for the ApiClient changes.

--- examples/react-router-dom/src/api.ts
+++ examples/react-router-dom/src/api.ts
@@ -2,8 +2,6 @@ import { redirect } from 'react-router-dom';
 import { type ErrorDetail, StorageClient } from '@jotsu/jotsu-js';
 
 class ApiClient extends StorageClient {
-    public accountId: string = process.env.REACT_APP_ACCOUNT_ID;
-
     constructor() {
         super(process.env.REACT_APP_ACCOUNT_ID);
     }
